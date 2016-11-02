4060ee6f318f392bf41b34e4c31cbccf113273a6
chad <chad.king@mindflash.com>
2016-11-02 14:03:21 -0700

    Modifying broke max-len rule config

--- rules/stylistic-issues.js
+++ rules/stylistic-issues.js
@@ -86,7 +86,6 @@ module.exports = {
 		// enforce a maximum line length
 		'max-len': [2, {
 			'code': 120,
-			'tabWidth': 4,
 			'ignoreUrls': true,
 			'ignoreComments': true,
 			'ignoreTrailingComments': true,
