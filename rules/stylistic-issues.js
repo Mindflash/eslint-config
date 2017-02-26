038a7395737d9771565dee0caaadf58e5c641625
Wesley Cho <wesley.cho@gmail.com>
2017-02-26 07:38:47 -0800

    feat(no-mixed-operators): disable rule
    - Disable `no-mixed-operators` rule as this adds nothing and forces unnecessary breakup of valid calculations

--- rules/stylistic-issues.js
+++ rules/stylistic-issues.js
@@ -142,7 +142,7 @@ module.exports = {
     'no-lonely-if': 2,
 
     // prevents the use of mixed logical operators with out parentheses
-    'no-mixed-operators': [2, {'allowSamePrecedence': true}],
+    'no-mixed-operators': 0,
 
     // prevents mixed indentation style
     'no-mixed-spaces-and-tabs': [2, 'smart-tabs'],
